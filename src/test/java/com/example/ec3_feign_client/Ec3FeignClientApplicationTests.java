b82fbfb65aeac08b2e8f07192885b14079ad5b3c
bluerrynwn <bluerrynwn@gmail.com>
2025-03-16 17:20:21 -0500

    primer commit

---
+++ src/test/java/com/example/ec3_feign_client/Ec3FeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec3_feign_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3FeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
